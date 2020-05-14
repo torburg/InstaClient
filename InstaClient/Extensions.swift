0b963fa189407ce2a42dafaaf7a9efe6bd10c13c
torburg <torburg@yandex.ru>
2020-05-15 01:35:48 +0300

    Gallery setted to display injected images with resizing

---
+++ InstaClient/Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Extensions.swift
+//  InstaClient
+//
+//  Created by Maksim Torburg on 15.05.2020.
+//  Copyright © 2020 Maksim Torburg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Images are too large to display correctly w/o resizing
+func resized(_ image: UIImage, to size: CGSize) -> UIImage {
+    let renderer = UIGraphicsImageRenderer(size: size)
+    let resizedImage = renderer.image { (context) in
+        image.draw(in: CGRect(origin: .zero, size: size))
+    }
+    return resizedImage
+}
+
--- InstaClient/ProfileViewControllerrViewController.swift
+++ InstaClient/ProfileViewControllerrViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class ProfileViewControllerrViewController: UIViewController {
 
+    @IBOutlet weak var numberOfPosts: UILabel!
+    @IBOutlet weak var numberOfFollowers: UILabel!
+    @IBOutlet weak var numberOfFollowing: UILabel!
+    
+    @IBOutlet weak var gallery: UICollectionView!
     @IBOutlet weak var avatar: UIImageView!
     @IBOutlet weak var editButton: UIButton!
+    
+    private let minimumSpacing: CGFloat = 3.0
+    private let imagesCount = 6
+    private let maxItemsInLine = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,8 +30,54 @@ class ProfileViewControllerrViewController: UIViewController {
 
     func setup() {
         avatar.layer.cornerRadius = avatar.frame.width / 2
+        
+        numberOfPosts.text = "0"
+        numberOfFollowers.text = "0"
+        numberOfFollowing.text = "0"
+        
         editButton.layer.cornerRadius = 5
         editButton.layer.borderWidth = 1
         editButton.layer.borderColor = UIColor(red: 0.859, green: 0.859, blue: 0.859, alpha: 1).cgColor
+        
+        gallery.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "reuseID")
     }
 }
+
+extension ProfileViewControllerrViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imagesCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reuseID", for: indexPath)
+        
+        guard let image = UIImage(named: "color_study") else { return cell }
+        let resizedImage = resized(image, to: cell.bounds.size)
+        let imageView = UIImageView(image: resizedImage)
+        imageView.contentMode = .scaleAspectFit
+
+        cell.contentView.addSubview(imageView)
+        cell.contentMode = .scaleAspectFit
+
+        return cell
+    }
+}
+
+extension ProfileViewControllerrViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let fullWidth = collectionView.bounds.width
+        let availableWidth = fullWidth - CGFloat(maxItemsInLine - 1) * minimumSpacing
+        let widthOfItem = availableWidth / CGFloat(maxItemsInLine)
+        return CGSize(width: widthOfItem, height: widthOfItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return minimumSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return minimumSpacing
+    }
+
+}
+
